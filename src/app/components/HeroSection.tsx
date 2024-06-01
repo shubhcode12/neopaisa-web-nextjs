ec410db3a66b41fe9f55ea8a99b10fcd6b46d3f9
shubhcode <shubhcode.dev@gmail.com>
2024-06-01 19:48:33 +0530

    add hero section

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from "next/image";
+import { MacbookScroll } from "../components/MacbookScroll";
+import Link from "next/link";
+
+
+export default function HeroSection() {
+  return (
+    <div id='heroSection' className="overflow-hidden dark:bg-[#0b0b0f] bg-white w-full">
+    <MacbookScroll
+      title={
+        <span>
+          Your Instant Loan Companion
+          <br /> NeoPaisa.com
+        </span>
+      }
+      badge={
+        <Link href="https://neopaisa.com/">
+          <Image
+            src="logo.svg"
+            width={30}
+            height={20}
+            alt="ss"
+            className="flex-shrink-0 rounded-md shadow-2xl"
+          />
+        </Link>
+      }
+      src={`/linear.png`}
+      showGradient={false}
+    />
+  </div>
+  )
+}
