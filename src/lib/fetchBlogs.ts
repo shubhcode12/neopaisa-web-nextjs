37100a26661854602b8f287c6ac75c4644c5cc58
shubhcode <shubhcode.dev@gmail.com>
2024-06-01 19:49:28 +0530

    fix fetch functions

--- src/lib/fetchBlogs.ts
+++ src/lib/fetchBlogs.ts
@@ -3,7 +3,13 @@ import { client } from "@/lib/prismic-configuration";
 export const fetchBlogs = async () => {
   try {
     const response = await client.getAllByType("blog");
-    return response;
+    return response.map((item: any)=>({
+      uid: item.data.uid,
+      title: item.data.title,
+      content: item.data.content,
+      category: item.data.category,
+      image: item.data.image.url,
+    }));
   } catch (error) {
     console.error("Error fetching blogs:", error);
     return [];
--- src/lib/fetchTestimonials.ts
+++ src/lib/fetchTestimonials.ts
@@ -4,8 +4,8 @@ export const fetchTestimonial = async () => {
   try {
     const response = await client.getAllByType("testimonial");
     return response.map((item: any) => ({
-      quote: item.data.quote,
-      name: item.data.name,
+      quote: item.data.review,
+      name: item.data.author,
       title: item.data.title,
     }));
   } catch (error) {
