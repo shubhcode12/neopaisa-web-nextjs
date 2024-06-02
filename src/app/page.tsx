e0f3ce4de1944e34c18e2a00656692a140624fb7
shubhcode <shubhcode.dev@gmail.com>
2024-06-02 18:16:26 +0530

    reformat main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,38 +8,29 @@ import { fetchTestimonial } from "@/lib/fetchTestimonials";
 import { BottomFooter } from "./components/BottomFooter";
 import BlogsSection from "./components/BlogsSection";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+import TestimonialSection from "./components/TestimonialSection";
 
 export default async function Home() {
-  const testimonialsData = await fetchTestimonial();
-  console.log(testimonialsData);
+  
 
   return (
-    <main className="flex min-h-screen bg-[#0b0b0f] flex-col items-center justify-between">
+    <main className="relative bg-[#0c0c10] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+
       <Navbar navItems={navItems} className="custom-classname" />
 
       <HeroSection/>
 
       <LampDemo />
 
-      {/* <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-        <InfiniteMovingCards
-          items={testimonialsData}
-          direction="right"
-          speed="slow"
-        />
-      </div> */}
-
-      {/* <div className="h-[40rem] rounded-md flex flex-col antialiased bg-white dark:bg-black dark:bg-grid-white/[0.05] items-center justify-center relative overflow-hidden">
-        <InfiniteMovingCards
-          items={testimonialsData}
-          direction="right"
-          speed="slow"
-        />
-      </div> */}
+      <TestimonialSection/>
 
       <BlogsSection />
 
-      <BottomFooter />
+    
+
+      {/* <BottomFooter /> */}
+      <Footer/>
     </main>
   );
 }
