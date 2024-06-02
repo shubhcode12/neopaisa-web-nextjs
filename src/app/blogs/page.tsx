4b6149da7716e099c31456b85a26e0b247b90a6e
shubhcode <shubhcode.dev@gmail.com>
2024-06-03 03:57:23 +0530

    clean code

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -5,7 +5,6 @@ import navItems from "../utils/NavItems";
 import BlogsSection from "../components/BlogsSection";
 
 export default async function Blogs() {
-  const blogs = await fetchBlogs();
 
   return (
     <main className="flex flex-col min-h-screen items-center justify-between">
