aa471c0084c5344659af1f1e6b4027271d483f66
shubhcode <shubhcode.dev@gmail.com>
2024-06-03 03:57:56 +0530

    clean code

--- src/app/utils/firebaseConfig.ts
+++ src/app/utils/firebaseConfig.ts
@@ -1,7 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
-
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -10,11 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
-
-console.log(firebaseConfig)
-
 
 export const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
--- src/lib/fetchBlogs.ts
+++ src/lib/fetchBlogs.ts
@@ -3,7 +3,7 @@ import { client } from "@/lib/prismic-configuration";
 export const fetchBlogs = async () => {
   try {
     const response = await client.getAllByType("blog");
-    return response.map((item: any)=>({
+    return response.map((item: any) => ({
       uid: item.uid,
       title: item.data.title,
       content: item.data.content,
@@ -13,5 +13,25 @@ export const fetchBlogs = async () => {
   } catch (error) {
     console.error("Error fetching blogs:", error);
     return [];
+  }
+};
+
+export const fetchBlogBySlug = async (slug: string) => {
+  try {
+    const response = await client.getByUID("blog", slug);
+    if (response) {
+      const myData = response;
+      return {
+        uid: myData.uid,
+        title: myData.data.title,
+        content: myData.data.content,
+        category: myData.data.category,
+        image: myData.data.image.url,
+      };
+    }
+    throw new Error("Blog not found");
+  } catch (error) {
+    console.error("Error fetching blog by slug:", error);
+    throw error;
   }
 };
