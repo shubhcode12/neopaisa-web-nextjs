d43a1dc0f34790609afd88e556e9b894603d1f71
shubhcode <shubhcode.dev@gmail.com>
2024-06-02 21:29:05 +0530

    add spotlight effect at hero

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { MacbookScroll } from "./ui/MacbookScroll";
 import Link from "next/link";
-
+import { Spotlight } from "./ui/Spotlight";
 
 export default function HeroSection() {
   return (
-    <div id='heroSection' className="overflow-hidden dark:bg-[#0b0b0f] bg-white w-full">
-    <MacbookScroll
-      title={
-        <span>
-          Your Instant Loan Companion
-          <br /> NeoPaisa.com
-        </span>
-      }
-      badge={
-        <Link href="https://neopaisa.com/">
-          <Image
-            src="logo.svg"
-            width={30}
-            height={20}
-            alt="ss"
-            className="flex-shrink-0 rounded-md shadow-2xl"
-          />
-        </Link>
-      }
-      src={`/linear.png`}
-      showGradient={false}
-    />
-  </div>
-  )
+    <div
+      id="heroSection"
+      className="overflow-hidden dark:bg-[#0b0b0f] bg-white w-full">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="#c3d740"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="#c3d740"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="#f6ffc4" />
+      </div>
+      <MacbookScroll
+        title={
+          <span>
+            Your Instant Loan Companion
+            <br /> NeoPaisa.com
+          </span>
+        }
+        badge={
+          <Link href="https://neopaisa.com/">
+            <Image
+              src="logo.svg"
+              width={30}
+              height={20}
+              alt="ss"
+              className="flex-shrink-0 rounded-md shadow-2xl"
+            />
+          </Link>
+        }
+        src={`/linear.png`}
+        showGradient={false}
+      />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ const config: Config = {
           "scroll var(--animation-duration, 100s) var(--animation-direction, forwards) linear infinite",
         marquee: "marquee 20s linear infinite",
         aurora: "aurora 60s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
       keyframes: {
         scroll: {
@@ -33,12 +34,14 @@ const config: Config = {
           "0%": { transform: "translateX(100%)" },
           "100%": { transform: "translateX(-100%)" },
         },
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
           },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
           },
         },
       },
