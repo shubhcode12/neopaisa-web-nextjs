712bbcd266fd0f2467ee5e4d7487e7b10fdf0616
shubhcode <shubhcode.dev@gmail.com>
2024-06-02 18:41:31 +0530

    add maque section

--- src/app/components/Marquee.tsx
+++ src/app/components/Marquee.tsx
@@ -4,8 +4,8 @@ const Marquee: React.FC = () => {
   return (
     <div className="overflow-hidden whitespace-nowrap bg-black p-16 text-[#c3d740]">
       <div className="flex animate-scroll">
-        <span className="text-8xl px-8">
-          - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;
+        <span className="text-6xl px-8">
+          - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;  - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;  - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;  - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;  - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;  - LET&apos;S TALK - &nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp; - GET IN TOUCH - &nbsp; - LET&apos;S TALK&nbsp;GET IN TOUCH&nbsp;
         </span>
       </div>
     </div>
