2942b6fbca7fbb6f221ce9a519b09087a15185ee
shubhcode <shubhcode.dev@gmail.com>
2024-06-02 18:30:01 +0530

    clean main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,26 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { LampDemo } from "./components/LampBg";
 import { Navbar } from "./components/NavBar";
 import navItems from "./utils/NavItems";
-import { InfiniteMovingCards } from "./components/InfiniteMovingCards";
-import { fetchTestimonial } from "@/lib/fetchTestimonials";
-import { BottomFooter } from "./components/BottomFooter";
 import BlogsSection from "./components/BlogsSection";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
 import TestimonialSection from "./components/TestimonialSection";
 
 export default async function Home() {
-  
-
   return (
     <main className="relative bg-[#0c0c10] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
-
       <Navbar navItems={navItems} className="custom-classname" />
 
-      <HeroSection/>
+      <HeroSection />
 
       <LampDemo />
 
-      <TestimonialSection/>
+      <TestimonialSection />
 
       <BlogsSection />
 
-    
-
-      {/* <BottomFooter /> */}
-      <Footer/>
+      <Footer />
     </main>
   );
 }
