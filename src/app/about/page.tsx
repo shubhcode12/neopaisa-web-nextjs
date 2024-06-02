75fe3b795b72a56eb9304f93472a441ded652914
shubhcode <shubhcode.dev@gmail.com>
2024-06-03 01:09:16 +0530

    add about us ui

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,42 @@
 import navItems from "../utils/NavItems";
 import { Navbar } from "../components/NavBar";
+import Marquee from "../components/Marquee";
+import { APP_PLAY_STORE_LINK } from "@/data";
+import Footer from "../components/Footer";
 
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="relative bg-[#0c0c10] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <Navbar navItems={navItems} className="custom-classname" />
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        this is about
+      <section
+        id="testimonials"
+        className="max-w-7xl mx-auto w-full relative overflow-hidden py-44 px-4">
+        <h2 className="text-center text-4xl md:text-4xl lg:text-7xl font-bold text-black dark:text-white">
+          We're building a world where money can flow effortlessly into the
+          things people truly care about.
+          <span className="text-[#c3d740]"></span>
+        </h2>
+        <p className="text-center text-base md:text-lg font-normal text-gray-500 max-w-md mt-2 mx-auto">
+          about us
+        </p>
+      </section>
+      <div className="overflow-hidden whitespace-nowrap bg-black p-16 text-[#c3d740]">
+        <div className="flex animate-scroll">
+          <a href={APP_PLAY_STORE_LINK}>
+            <span className="text-6xl font-bold px-8">
+              - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW -
+              DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW -
+              DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW -
+              DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW -
+              DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW -
+              DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW - DOWNLOAD NOW
+            </span>
+          </a>
+        </div>
       </div>
+
+      <Footer/>
     </main>
   );
 }
